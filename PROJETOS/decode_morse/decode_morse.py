bdc9787cfe66a086d28142789d088b1aa94913c7
Esther Andrade Brasileiro <brasileiroesther@gmail.com>
2024-09-10 19:54:35 -0300

    sobe projeto 01

--- PROJETOS/decode_morse/decode_morse.py
+++ PROJETOS/decode_morse/decode_morse.py
@@ -7,34 +7,57 @@ O script tem a finalidade de decifrar uma mensagem em código morse e salvá-la 
 '''
 
 import os
-import sys
 import datetime
 import pandas as pd
 from config import file_path, dict_morse
 
 def decode_morse(msg):
-    '''
-    input : mensagem em código morse com as letras separadas por espaços
-    output : palavra escrito em letras e algarismos
-    '''
-    msg_lst = msg.split(" ")
-    msg_claro = []
-    for letter in msg_lst:
-        msg_claro.append(dict_morse[letter])
-    return "".join(msg_claro)
+    """
+    Decodificando uma mensagem em código Morse, seguindo os critérios:
+      - As letras estão separadas por um espaço simples
+      - As palavras estão separadas por dois espaços
+    """
+    words = msg.split("  ")
+    decoded_words = []
 
-def save_clear_msg_csv_hdr(msg_claro):
-    '''
-    input : mensagem em texto claro
-    output : palavra escrito em letras e algarismos, salva em arquivo csv
-    '''
+    for word in words:
+        letters = word.split(" ")
+        decoded_word = []
+        for letter in letters:
+            if letter in dict_morse:
+                decoded_word.append(dict_morse[letter])
+            else:
+                decoded_word.append('?') 
+        decoded_words.append("".join(decoded_word))
+
+    return " ".join(decoded_words)
+
+def save_clear_msg_csv_hdr(morse_code, msg_claro):
+    """
+    Salvando a mensagem em Morse e o texto decodificado em um CSV com a data e hora.
+    """
     now = datetime.datetime.now()
-    df = pd.DataFrame([[msg_claro, now]], columns=["mensagem", "datetime"])
+    df = pd.DataFrame([[morse_code, msg_claro, now]], columns=["codigo_morse", "mensagem_decodificada", "datetime"])
+    
+    # Verificando se o CSV já existe
     hdr = not os.path.exists(file_path)
-    df.to_csv(file_path, mode ="a", index = False, header=hdr)
+    
+    # Salvando
+    df.to_csv(file_path, mode="a", index=False, header=hdr)
 
 if __name__ == "__main__":
-    msg_claro = decode_morse(sys.argv[1])
-    save_clear_msg_csv_hdr(msg_claro)
-    #print(save_clear_msg_csv_hdr.__doc__)
-    #print(pd.to_pickle.__doc__)
+
+    tres_frases = [
+        ".... . .-.. .-.. ---  .-- --- .-. .-.. -.. -.-.--",
+        ". ... - ..- -.. .- -. -.. ---  .--. -.-- - .... --- -.",
+        "... --- ...  .--. -.-- - .... --- -."
+    ]
+
+    for i, frase in enumerate(tres_frases, 1):
+        print(f"Decodificando frase {i}:")
+        msg_claro = decode_morse(frase)
+        print(f"Mensagem decodificada: {msg_claro}")
+        
+        #Salvando no CSV o código Morse e a mensagem decodificada
+        save_clear_msg_csv_hdr(frase, msg_claro)
+        print(f"Mensagem e código Morse salvos no CSV.\n")
